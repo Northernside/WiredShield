82506a0e2beff6aef5d3413ed3427e9091bdab2f
Elias <github@northernsi.de>
2024-12-16 08:41:20 +0100

    test?

--- http/http.go
+++ http/http.go
@@ -1,80 +1,80 @@
 package http
 
 import (
+	"bytes"
 	"crypto/tls"
 	"database/sql"
 	"encoding/json"
 	"fmt"
 	"sync"
 	"time"
-
 	"wiredshield/modules/db"
 	"wiredshield/modules/env"
 	"wiredshield/services"
 
-	_ "github.com/lib/pq"
 	"github.com/valyala/fasthttp"
 )
 
+type RequestLog struct {
+	RequestTime          time.Time       `json:"request_time"`
+	ClientIP             string          `json:"client_ip"`
+	Method               string          `json:"method"`
+	Host                 string          `json:"host"`
+	Path                 string          `json:"path"`
+	QueryParams          string          `json:"query_params"`
+	RequestHeaders       json.RawMessage `json:"request_headers"`
+	ResponseHeaders      json.RawMessage `json:"response_headers"`
+	ResponseStatusOrigin int             `json:"response_status_origin"`
+	ResponseStatusProxy  int             `json:"response_status_proxy"`
+	ResponseTime         time.Duration   `json:"response_time"`
+	TLSVersion           string          `json:"tls_version"`
+	RequestSize          int64           `json:"request_size"`
+	ResponseSize         int64           `json:"response_size"`
+	RequestHTTPVersion   string          `json:"request_http_version"`
+}
+
 var (
-	requestsChannel = make(chan *fasthttp.RequestCtx, 1000)
-	requestBuffer   = make([][]interface{}, 0, 1000)
-	bufferLock      sync.Mutex
-	certCache       sync.Map
-	targetCache     sync.Map
-	clientPool      sync.Pool
-	service         *services.Service
-	dbConn          *sql.DB
+	requestsChannel    = make(chan *fasthttp.Request)
+	requestLogsChannel = make(chan *RequestLog, 1024*16)
+	clientPool         sync.Pool
+	service            *services.Service
+	certCache          sync.Map
+	dbConn             *sql.DB
 )
+
+func init() {
+	var err error
+	dbConn, err = sql.Open("postgres", fmt.Sprintf(
+		"user=%s password=%s dbname=%s sslmode=disable",
+		env.GetEnv("PSQL_USER", "wiredshield"),
+		env.GetEnv("PSQL_PASSWORD", ""),
+		env.GetEnv("PSQL_DB", "reverseproxy"),
+	))
+	if err != nil {
+		panic(fmt.Sprintf("Failed to connect to database: %v", err))
+	}
+
+	go processRequestLogs()
+}
 
 func Prepare(_service *services.Service) func() {
 	service = _service
 
-	var err error
-	dbConn, err = sql.Open("postgres", fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", env.GetEnv("PSQL_USER", "wiredshield"), env.GetEnv("PSQL_PASSWORD", ""), env.GetEnv("PSQL_DB", "reverseproxy")))
-	if err != nil {
-		service.FatalLog(fmt.Sprintf("database connection failed: %v", err))
-	}
-
-	_, err = dbConn.Exec(`
-	CREATE TABLE IF NOT EXISTS requests (
-		id SERIAL PRIMARY KEY,
-		request_time TIMESTAMPTZ,
-		client_ip INET,
-		method TEXT,
-		host TEXT,
-		path TEXT,
-		query_params JSONB,
-		request_headers JSONB,
-		response_headers JSONB,
-		response_status_origin SMALLINT,
-		response_status_proxy SMALLINT,
-		response_time INTERVAL,
-		tls_version TEXT,
-		request_size BIGINT,
-		response_size BIGINT,
-		request_http_version TEXT
-	) `)
-	if err != nil {
-		service.FatalLog(fmt.Sprintf("Failed to ensure requests table exists: %v", err))
-	}
-
-	go processRequests()
-	go flushRequestBuffer()
-
-	clientPool.New = func() interface{} {
-		return &fasthttp.Client{
-			ReadTimeout:  30 * time.Second,
-			WriteTimeout: 30 * time.Second,
-		}
-	}
+	go handleRequestLogging()
 
 	return func() {
 		port := env.GetEnv("HTTP_PORT", "443")
 		binding := env.GetEnv("HTTP_BINDING", "0.0.0.0")
 		addr := binding + ":" + port
 
-		service.InfoLog(fmt.Sprintf("Starting proxy on %s", addr))
+		clientPool.New = func() interface{} {
+			return &fasthttp.Client{
+				ReadTimeout:  30 * time.Second,
+				WriteTimeout: 30 * time.Second,
+			}
+		}
+
+		service.InfoLog("Starting HTTPS proxy on " + addr)
 		service.OnlineSince = time.Now().Unix()
 
 		server := &fasthttp.Server{
@@ -99,24 +99,13 @@ func Prepare(_service *services.Service) func() {
 
 func ProxyHandler(ctx *fasthttp.RequestCtx) {
 	timeStart := time.Now()
-	host := string(ctx.Host())
-	var targetURL string
-	if targetURL, found := targetCache.Load(host); found {
-		targetURL = targetURL.(string)
-	} else {
-		targetRecords, err := db.GetRecords("A", host)
-		if err != nil || len(targetRecords) == 0 {
-			ctx.Error(fmt.Sprintf("no records found for %s\ndebug: %v\ntargetRecords: %v\nhost: %s", host, err, targetRecords, host), fasthttp.StatusNotFound)
-			return
-		}
-
-		targetRecord := targetRecords[0].(db.ARecord)
-		targetURL := "http://" + targetRecord.IP + string(ctx.Path())
-		targetCache.Store(host, targetURL)
-		time.AfterFunc(1*time.Hour, func() {
-			targetCache.Delete(host)
-		})
+	targetRecords, err := db.GetRecords("A", string(ctx.Host()))
+	if err != nil || len(targetRecords) == 0 {
+		ctx.Error("could not resolve target", fasthttp.StatusBadGateway)
+		return
 	}
+	targetRecord := targetRecords[0].(db.ARecord)
+	targetURL := "http://" + targetRecord.IP + string(ctx.Path())
 
 	req := fasthttp.AcquireRequest()
 	defer fasthttp.ReleaseRequest(req)
@@ -136,7 +125,7 @@ func ProxyHandler(ctx *fasthttp.RequestCtx) {
 	client := clientPool.Get().(*fasthttp.Client)
 	defer clientPool.Put(client)
 
-	err := client.Do(req, resp)
+	err = client.Do(req, resp)
 	if err != nil {
 		ctx.Error("error contacting backend", fasthttp.StatusBadGateway)
 		return
@@ -151,124 +140,106 @@ func ProxyHandler(ctx *fasthttp.RequestCtx) {
 	ctx.SetStatusCode(resp.StatusCode())
 	ctx.SetBody(resp.Body())
 
+	reqHeaders, _ := json.Marshal(convertHeaders(&ctx.Request.Header))
+	respHeaders, _ := json.Marshal(convertHeaders(&resp.Header))
+
+	requestLogsChannel <- &RequestLog{
+		RequestTime:          timeStart,
+		ClientIP:             ctx.RemoteAddr().String(),
+		Method:               string(ctx.Method()),
+		Host:                 string(ctx.Host()),
+		Path:                 string(ctx.Path()),
+		QueryParams:          string(ctx.URI().QueryString()),
+		RequestHeaders:       reqHeaders,
+		ResponseHeaders:      respHeaders,
+		ResponseStatusOrigin: resp.StatusCode(),
+		ResponseStatusProxy:  ctx.Response.StatusCode(),
+		ResponseTime:         time.Since(timeStart),
+		TLSVersion:           tlsVersionToString(ctx.TLSConnectionState().Version),
+		RequestSize:          int64(ctx.Request.Header.ContentLength()),
+		ResponseSize:         int64(resp.Header.ContentLength()),
+		RequestHTTPVersion:   string(ctx.Request.Header.Protocol()),
+	}
+
 	select {
-	case requestsChannel <- ctx:
+	case requestsChannel <- req:
 	default:
+		ctx.Error("service unavailable", fasthttp.StatusServiceUnavailable)
 	}
 }
 
-func buildRequestRow(ctx *fasthttp.RequestCtx, resp *fasthttp.Response, responseTime time.Duration) []interface{} {
-	requestHeaders := make(map[string]string)
-	ctx.Request.Header.VisitAll(func(k, v []byte) {
-		requestHeaders[string(k)] = string(v)
-	})
-
-	responseHeaders := make(map[string]string)
-	resp.Header.VisitAll(func(k, v []byte) {
-		responseHeaders[string(k)] = string(v)
-	})
-
-	queryParams := make(map[string]string)
-	ctx.QueryArgs().VisitAll(func(k, v []byte) {
-		queryParams[string(k)] = string(v)
-	})
-
-	requestSize := len(ctx.Request.Body())
-	responseSize := len(resp.Body())
-	tlsVersion := "unknown"
-	if ctx.IsTLS() {
-		if tlsConnState := ctx.TLSConnectionState(); tlsConnState != nil {
-			switch tlsConnState.Version {
-			case tls.VersionTLS10:
-				tlsVersion = "TLS 1.0"
-			case tls.VersionTLS11:
-				tlsVersion = "TLS 1.1"
-			case tls.VersionTLS12:
-				tlsVersion = "TLS 1.2"
-			case tls.VersionTLS13:
-				tlsVersion = "TLS 1.3"
-			}
-		}
-	} else {
-		tlsVersion = "none"
+func convertHeaders(headers interface{}) map[string]string {
+	result := make(map[string]string)
+	switch h := headers.(type) {
+	case *fasthttp.RequestHeader:
+		h.VisitAll(func(key, value []byte) {
+			result[string(key)] = string(value)
+		})
+	case *fasthttp.ResponseHeader:
+		h.VisitAll(func(key, value []byte) {
+			result[string(key)] = string(value)
+		})
 	}
 
-	return []interface{}{
-		time.Now().UTC(),
-		ctx.RemoteAddr().String(),
-		string(ctx.Method()),
-		string(ctx.Host()),
-		string(ctx.Path()),
-		jsonMapToJSON(queryParams),
-		jsonMapToJSON(requestHeaders),
-		jsonMapToJSON(responseHeaders),
-		resp.StatusCode(),
-		ctx.Response.StatusCode(),
-		responseTime,
-		tlsVersion,
-		requestSize,
-		responseSize,
-		string(ctx.Request.Header.Protocol()),
+	return result
+}
+
+func tlsVersionToString(version uint16) string {
+	switch version {
+	case tls.VersionTLS10:
+		return "TLS v1.0"
+	case tls.VersionTLS11:
+		return "TLS v1.1"
+	case tls.VersionTLS12:
+		return "TLS v1.2"
+	case tls.VersionTLS13:
+		return "TLS v1.3"
+	default:
+		return "Unknown"
 	}
 }
 
-func processRequests() {
+func handleRequestLogging() {
 	for req := range requestsChannel {
-		timeStart := time.Now()
-
-		if req != nil {
-			resp := fasthttp.AcquireResponse()
-			bufferLock.Lock()
-			requestBuffer = append(requestBuffer, buildRequestRow(req, resp, time.Since(timeStart)))
-			bufferLock.Unlock()
-			fasthttp.ReleaseResponse(resp)
-		} else {
-			service.ErrorLog("nil request received")
-		}
+		service.InfoLog("Request to " + string(req.URI().Host()))
 	}
 }
 
-func flushRequestBuffer() {
-	ticker := time.NewTicker(5 * time.Minute)
+func processRequestLogs() {
+	ticker := time.NewTicker(5 * time.Second)
 	defer ticker.Stop()
 
 	for range ticker.C {
-		bufferLock.Lock()
-		if len(requestBuffer) > 0 {
-			flushBuffer := requestBuffer
-			requestBuffer = make([][]interface{}, 0, 1000)
-			bufferLock.Unlock()
+		flushRequestLogs()
+	}
+}
 
-			tx, err := dbConn.Begin()
+func flushRequestLogs() {
+	var logsBuffer bytes.Buffer
+
+	for {
+		select {
+		case log := <-requestLogsChannel:
+			logLine, err := json.Marshal(log)
 			if err != nil {
-				service.ErrorLog(fmt.Sprintf("Failed to start transaction: %v", err))
+				service.WarnLog(fmt.Sprintf("Failed to marshal log: %v", err))
 				continue
 			}
-
-			stmt, err := tx.Prepare(`
-			COPY requests (request_time, client_ip, method, host, path, query_params, request_headers, response_headers, response_status_origin, response_status_proxy, response_time, tls_version, request_size, response_size, request_http_version) 
-			FROM STDIN`)
-			if err != nil {
-				service.ErrorLog(fmt.Sprintf("Failed to prepare COPY statement: %v", err))
-				tx.Rollback()
-				continue
-			}
-
-			for _, row := range flushBuffer {
-				_, err := stmt.Exec(row...)
-				if err != nil {
-					service.ErrorLog(fmt.Sprintf("Failed to execute COPY statement: %v", err))
-					tx.Rollback()
-					break
-				}
-			}
-
-			if err := tx.Commit(); err != nil {
-				service.ErrorLog(fmt.Sprintf("Failed to commit transaction: %v", err))
-			}
-		} else {
-			bufferLock.Unlock()
+			logsBuffer.Write(logLine)
+			logsBuffer.WriteByte('\n')
+		default:
+			goto INSERT
 		}
+	}
+
+INSERT:
+	if logsBuffer.Len() == 0 {
+		return
+	}
+
+	_, err := dbConn.Exec(`COPY requests (data) FROM STDIN`, logsBuffer.String())
+	if err != nil {
+		service.WarnLog(fmt.Sprintf("Failed to insert logs: %v", err))
 	}
 }
 
@@ -278,28 +249,15 @@ func getCertificateForDomain(hello *tls.ClientHelloInfo) (*tls.Certificate, erro
 		return nil, fmt.Errorf("no SNI provided by client")
 	}
 
-	cachedCert, ok := certCache.Load(domain)
-	if ok {
-		cert, ok := cachedCert.(*tls.Certificate)
-		if !ok {
-			return nil, fmt.Errorf("cached certificate for domain '%s' is of incorrect type", domain)
-		}
-
-		return cert, nil
+	if cert, ok := certCache.Load(domain); ok {
+		return cert.(*tls.Certificate), nil
 	}
 
-	service.InfoLog("loading certificate for " + domain)
+	service.InfoLog("Loading certificate for " + domain)
 	c, err := tls.LoadX509KeyPair(fmt.Sprintf("certs/%s.crt", domain), fmt.Sprintf("certs/%s.key", domain))
-	if err != nil {
-		return nil, fmt.Errorf("failed to load certificate for domain '%s': %v", domain, err)
+	if err == nil {
+		certCache.Store(domain, &c)
 	}
-
-	certCache.Store(domain, &c)
 
 	return &c, err
-}
-
-func jsonMapToJSON(data map[string]string) []byte {
-	result, _ := json.Marshal(data)
-	return result
 }
