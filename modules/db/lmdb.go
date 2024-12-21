54438064a8ed078f97f250805b8e44eb7c6690f8
Elias <github@northernsi.de>
2024-12-21 22:37:08 +0100

    test #3 reverted http logic

--- modules/db/lmdb.go
+++ modules/db/lmdb.go
@@ -19,10 +19,10 @@ func Init() {
 		log.Fatal("failed to create LMDB environment:", err)
 	}
 
-	// err = env.SetMaxReaders(1024 * 32)
-	// if err != nil {
-	// 	log.Fatal("failed to set max readers:", err)
-	// }
+	err = env.SetMaxReaders(1024 * 32)
+	if err != nil {
+		log.Fatal("failed to set max readers:", err)
+	}
 
 	err = env.SetMaxDBs(2 ^ 32 - 1)
 	if err != nil {
