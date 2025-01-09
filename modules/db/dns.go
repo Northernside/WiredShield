cc11f42502fb9289d888055ce0f19a2f010e2fa5
Elias <github@northernsi.de>
2025-01-09 20:16:09 +0100

    aa

--- modules/db/dns.go
+++ modules/db/dns.go
@@ -377,7 +377,7 @@ func GetRecords(recordType, domain string) ([]DNSRecord, error) {
 				return fmt.Errorf("failed to deserialize record: %w", err)
 			}
 
-			services.ProcessService.InfoLog(fmt.Sprintf("record: %v", record))
+			services.ProcessService.InfoLog(fmt.Sprintf("record: %v", &record))
 
 			// check the record type
 			if record.GetType() == recordType {
