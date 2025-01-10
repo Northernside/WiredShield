20c60dd3a94e81bbfbe2d763fbfe593879bd3c0a
Northernside <gpg@northernsi.de>
2025-01-10 01:54:20 +0100

    mhmm

--- modules/db/dns.go
+++ modules/db/dns.go
@@ -377,11 +377,12 @@ func GetRecords(recordType, domain string) ([]DNSRecord, error) {
 				return fmt.Errorf("failed to deserialize record: %w", err)
 			}
 
-			services.ProcessService.InfoLog(fmt.Sprintf("record: %v", record))
+			services.ProcessService.InfoLog(fmt.Sprintf("record: %+v", record))
 
 			// check the record type
 			if record.GetType() == recordType {
-				records = append(records, record)
+				copiedRecord := record
+				records = append(records, copiedRecord)
 			}
 		}
 
