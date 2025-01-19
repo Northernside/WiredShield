59fe672e6be810545fc2d49bd5b42310f049b437
Northernside <gpg@northernsi.de>
2025-01-19 02:43:45 +0100

    getrecords

---
+++ http/routes/api/domains/getrecords.go
@@ -0,0 +1,39 @@
+package routes
+
+import (
+	"encoding/json"
+	"wiredshield/modules/db"
+	pages "wiredshield/pages"
+
+	"github.com/valyala/fasthttp"
+)
+
+func GetRecords(ctx *fasthttp.RequestCtx) {
+	list, err := db.GetRecordsByDomain(string(ctx.QueryArgs().Peek("domain")))
+	if err != nil {
+		var errorLines []string
+		errorLines = append(errorLines, pages.Error500...)
+		errorLines = append(errorLines, err.Error())
+		errorPage := pages.ErrorPage{Code: 500, Message: errorLines}
+		ctx.SetStatusCode(fasthttp.StatusInternalServerError)
+		ctx.Response.Header.Set("Content-Type", "text/html")
+		ctx.SetBodyString(errorPage.ToHTML())
+		return
+	}
+
+	ctx.Response.Header.Set("Content-Type", "application/json")
+	ctx.SetStatusCode(fasthttp.StatusOK)
+	jsonList, err := json.Marshal(list)
+	if err != nil {
+		var errorLines []string
+		errorLines = append(errorLines, pages.Error500...)
+		errorLines = append(errorLines, err.Error())
+		errorPage := pages.ErrorPage{Code: 500, Message: errorLines}
+		ctx.SetStatusCode(fasthttp.StatusInternalServerError)
+		ctx.Response.Header.Set("Content-Type", "text/html")
+		ctx.SetBodyString(errorPage.ToHTML())
+		return
+	}
+
+	ctx.SetBody(jsonList)
+}
