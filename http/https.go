ae14a405447081c7a3442f056b002e5f32f4756e
Elias <github@northernsi.de>
2025-02-06 14:35:44 +0100

    path fix probably

--- http/https.go
+++ http/https.go
@@ -241,7 +241,9 @@ func httpHandler(ctx *fasthttp.RequestCtx) {
 				protocol += "s"
 			}
 
-			target := fmt.Sprintf("%s://%s:%d%s", protocol, passthrough.TargetAddr, passthrough.TargetPort, string(ctx.Path()))
+			// ctx.Path but minus passthrough.Path
+			normalizedPath := string(ctx.Path())[len(passthrough.Path):]
+			target := fmt.Sprintf("%s://%s:%d%s", protocol, passthrough.TargetAddr, passthrough.TargetPort, normalizedPath)
 			// httpsProxyHandler(ctx *fasthttp.RequestCtx)
 			ctx.SetUserValue("targetURL", target)
 			ctx.SetUserValue("resolve", true)
