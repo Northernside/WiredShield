a60b45d51e4533cc8be1ed04ef402b040654110c
Elias <github@northernsi.de>
2025-02-07 12:47:40 +0100

    acme http challenge fixes

--- http/routes/.well-known/acme_challenge.go
+++ http/routes/.well-known/acme_challenge.go
@@ -1,18 +1,33 @@
 package routes
 
 import (
+	"strings"
 	"wiredshield/modules/db"
+	errorpages "wiredshield/pages/error"
 
 	"github.com/valyala/fasthttp"
 )
 
 func ACMEChallenge(ctx *fasthttp.RequestCtx) {
-	token := ctx.UserValue("tokeb").(string)
+	var token = strings.Split(string(ctx.Path()), "/")[len(strings.Split(string(ctx.Path()), "/"))-1]
 
 	// get the challenge from db
 	httpChallenge, err := db.GetHttpChallenge(token)
 	if err != nil {
-		ctx.Error(err.Error(), fasthttp.StatusInternalServerError)
+		if strings.Contains(err.Error(), "MDB_NOTFOUND") {
+			errorPage := errorpages.ErrorPage{Code: 404, Message: errorpages.Error604}
+			ctx.SetStatusCode(fasthttp.StatusNotFound)
+			ctx.Response.Header.Set("Content-Type", "text/html")
+			ctx.SetBodyString(errorPage.ToHTML())
+			return
+		}
+
+		errorPage := errorpages.ErrorPage{Code: 500, Message: errorpages.Error500}
+		errorPage.Message = append(errorPage.Message, err.Error())
+
+		ctx.SetStatusCode(fasthttp.StatusInternalServerError)
+		ctx.Response.Header.Set("Content-Type", "text/html")
+		ctx.SetBodyString(errorPage.ToHTML())
 		return
 	}
 
