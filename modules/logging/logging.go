8a70697adb24cd6da4b9173889f5e0471a5abf44
Elias <github@northernsi.de>
2025-02-17 11:22:07 +0100

    ?

--- modules/logging/logging.go
+++ modules/logging/logging.go
@@ -128,7 +128,7 @@ func (log *HTTPRequestLog) BatchInsert(logs []*HTTPRequestLog) error {
 	_, err = transaction.Exec(context.Background(), query, values...)
 	if err != nil {
 		if strings.Contains(err.Error(), "request_time") {
-			services.ProcessService.ErrorLog(fmt.Sprintf("%s", values))
+			services.ProcessService.ErrorLog(fmt.Sprintf("%s -------- %s", query, values))
 		}
 		return fmt.Errorf("batch insert failed: %v", err)
 	}
