4ccaad1f8ff9c1205fdc059e6fa0cf7803a8a702
Elias <github@northernsi.de>
2025-02-21 11:06:46 +0100

    d

--- modules/db/dns.go
+++ modules/db/dns.go
@@ -413,11 +413,16 @@ func GetRecords(recordType, domain string) ([]DNSRecord, error) {
 			services.ProcessService.InfoLog(fmt.Sprintf("%s, %s", recordType, domain))
 			entryData, err := txn.Get(entries, uint64ToByteArray(id))
 			if err != nil {
+				services.ProcessService.InfoLog(fmt.Sprintf("Failed to get entry data for id: %v, error: %v", id, err))
 				if errors.Is(err, lmdb.NotFound) {
 					continue // skip missing records
 				}
 
 				return fmt.Errorf("failed to fetch record: %w", err)
+			}
+
+			if recordType == "SRV" {
+				services.ProcessService.InfoLog(fmt.Sprintf("SRV record gfdzgszdg: %s", string(entryData)))
 			}
 
 			// deserialize the record
